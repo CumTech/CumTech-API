78417e14de96c30f79a084ca65b5f383b1aed060
LurchingDart <luizalferez25@gmail.com>
2023-03-22 08:27:34 -0600

    main

--- main.js
+++ main.js
@@ -13,35 +13,35 @@ app.use(cors())
 
 //-----------------Routes-----------------//
 const addressRoute = require('./routes/addressRoute');
-// const categoryRoute = require('./routes/categoryRoute');
-const orderDetailRoute = require('./routes/orderDetailRoute');
-// const orderRoute = require('./routes/orderRoute');
-// const productRoute = require('./routes/productRoute');
-// const userRoute = require('./routes/userRoute');
-// const wishlistRoute = require('./routes/wishlistRoute');
-// const addressRoute = require('./routes/addressRoute');
-// const categoryRoute = require('./routes/categoryRoute');
+const categoryRoute = require('./routes/categoryRoute');
 // const orderDetailRoute = require('./routes/orderDetailRoute');
-// const orderRoute = require('./routes/orderRoute');
-// const productRoute = require('./routes/productRoute');
-// const userRoute = require('./routes/userRoute');
-// const wishlistRoute = require('./routes/wishlistRoute');
+const orderRoute = require('./routes/orderRoute');
+const productRoute = require('./routes/productRoute');
+const userRoute = require('./routes/userRoute');
+const wishlistRoute = require('./routes/wishlistRoute');
+const addressRoute = require('./routes/addressRoute');
+const categoryRoute = require('./routes/categoryRoute');
+const orderDetailRoute = require('./routes/orderDetailRoute');
+const orderRoute = require('./routes/orderRoute');
+const productRoute = require('./routes/productRoute');
+const userRoute = require('./routes/userRoute');
+const wishlistRoute = require('./routes/wishlistRoute');
 
 //-----------------consumo de rutas-----------------//
 app.use("/address", addressRoute);
-// app.use("/category", categoryRoute);
-app.use("/orderdetail", orderDetailRoute);
-// app.use("/order", orderRoute);
-// app.use("/product", productRoute);
-// app.use("/user", userRoute);
-// app.use("/wishlist", wishlistRoute);
-// app.use("/address", addressRoute);
-// app.use("/category", categoryRoute);
+app.use("/category", categoryRoute);
 // app.use("/orderdetail", orderDetailRoute);
-// app.use("/order", orderRoute);
-// app.use("/product", productRoute);
-// app.use("/user", userRoute);
-// app.use("/wishlist", wishlistRoute);
+app.use("/order", orderRoute);
+app.use("/product", productRoute);
+app.use("/user", userRoute);
+app.use("/wishlist", wishlistRoute);
+app.use("/address", addressRoute);
+app.use("/category", categoryRoute);
+app.use("/orderdetail", orderDetailRoute);
+app.use("/order", orderRoute);
+app.use("/product", productRoute);
+app.use("/user", userRoute);
+app.use("/wishlist", wishlistRoute);
 
 //-----------------Ruta raíz-----------------//
 app.get("/", (req, res) => {
