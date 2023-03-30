d01c92d70a8e3cceeb11a278dde132a908bc258b
LurchingDart <luizalferez25@gmail.com>
2023-03-30 15:09:33 -0600

    Endpoint para filtrar por usuario

--- routes/orderdetailRoute.js
+++ routes/orderdetailRoute.js
@@ -36,6 +36,27 @@ routes.get('/:id', async (req, res) => {
     }
 });
 
+routes.get('/user/:userId', async (req, res) => {
+    const connection = await mongoose.createConnection(urlDB);
+    try {
+        const OrderDetailsModel = OrderDetailsModelCreator(connection);
+        const orderDetails = await OrderDetailsModel.find()
+            .populate({
+                path: 'order',
+                match: { user: req.params.userId }
+            })
+            .exec();
+        const filteredOrderDetails = orderDetails.filter(od => od.order !== null);
+        res.json(filteredOrderDetails);
+        connection.close();
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+        res.json({error: 'Internal server error'});
+        res.json({message: error.message})
+    }
+});
+
 routes.post('/', async (req, res) => {
     const connection = await mongoose.createConnection(urlDB);
     try {
